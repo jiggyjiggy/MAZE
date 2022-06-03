8c5e96b29268f412adb0aff1890895abba06aa8f
jiggy <jiggyjiggy0323@gmail.com>
2022-06-04 01:17:38 +0900

    refactor: Fix zcode->region;Cafe

--- cafes/models.py
+++ cafes/models.py
@@ -9,7 +9,7 @@ class Cafe(TimeStampModel):
     latitude                = models.DecimalField(max_digits=17, decimal_places=14)
     longitude               = models.DecimalField(max_digits=17, decimal_places=14)
     category                = models.ForeignKey("commons.Category", on_delete=models.PROTECT)
-    zcode                   = models.ForeignKey("commons.Region", on_delete=models.PROTECT)
+    region                  = models.ForeignKey("commons.Region", on_delete=models.PROTECT)
 
     class Meta:
         db_table = "cafes"
