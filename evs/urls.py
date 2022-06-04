ebf9c93cb8b9bafc9705dbce4dbfa7042ed71695
jiggy <jiggyjiggy0323@gmail.com>
2022-06-05 00:02:37 +0900

    feat: Create search nearest EV view

--- evs/urls.py
+++ evs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from evs.views   import EVMapView, EVAdminView
+from evs.views   import EVMapView, SearchNearestEV,EVAdminView
 
 urlpatterns = [
     path("", EVMapView.as_view()),
+    path("/nearest", SearchNearestEV.as_view()),
     path("/admin", EVAdminView.as_view())
 ]
--- evs/views.py
+++ evs/views.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from haversine import haversine
+
 from django.http      import JsonResponse
 from django.views     import View
 from django.db.models import Q, Count, Case, When
@@ -93,6 +95,51 @@ class EVMapView(View):
                 } for charger in station.charger_set.all()]
             }
         } for station in near_stations]
+
+        return JsonResponse({"results" : results}, status=200)
+
+
+class SearchNearestEV(View):
+    def get(self, request):
+        # 1 degree of longitude = 111.19 km
+        # 1 degree of latitude in seoul (longitude: 37 degree) = 88.80 km
+        LATITUDE_100m  = 0.0008993614533681087 
+        LONGITUDE_100m = 0.0011261261261261261
+
+        user_latitude  = float(request.GET.get("user_latitude", None))
+        user_longitude = float(request.GET.get("user_longitude", None)) 
+        user_position  = (user_latitude, user_longitude)
+
+        nearest_station = True
+        offset          = 0
+
+        while nearest_station:
+            offset += 1
+            search_range = (
+                    Q(latitude__range  = (user_latitude - LATITUDE_100m * offset, user_latitude + LATITUDE_100m * offset)) &
+                    Q(longitude__range = (user_longitude - LONGITUDE_100m * offset, user_longitude + LONGITUDE_100m * offset))
+                )
+
+            stations = Station.objects.filter(search_range)
+            
+            distances = [haversine(user_position, (station.latitude, station.longitude)) for station in stations]
+            if distances:
+                break
+
+        nearest_distance = min(distances)
+        nearest_station  = stations[distances.index(nearest_distance)]
+
+        results = {
+            "search_range" : {
+                "km" : 0.1 * offset
+            },
+            "nearest station": {
+                    "distance"  : nearest_distance,
+                    "id"        : nearest_station.id,
+                    "latitude"  : nearest_station.latitude,
+                    "longitude" : nearest_station.longitude
+                }
+            }
 
         return JsonResponse({"results" : results}, status=200)
 
